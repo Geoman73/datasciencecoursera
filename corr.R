a16adb280cf2adc02d29fa83f6bc61935a8336e0
Georman73 <gjforgey@gmail.com>
2017-06-01 18:03:58 -0500

    Update corr.R

--- corr.R
+++ corr.R
@@ -1,23 +1,17 @@
 corr <- function(directory, threshold = 0) {
-  if(grep("specdata", directory) == 1) {
-    directory <- ("./specdata/")
-  }
   complete_table <- complete("specdata", 1:332)
   nobs <- complete_table$nobs
-  ids <- complete_table$id[nobs > threshold]
-  id_len <- length(ids)
-  corr_vector <- rep(0, id_len)
-  all_files <- as.character( list.files(directory) )
-  file_paths <- paste(directory, all_files, sep="")
+  idnums <- complete_table$id[nobs > threshold]
+  corrdata <- numeric()
+  fileList <- list.files(path = directory, pattern = ".csv", full.names = TRUE)
   j <- 1
-  for(i in ids) {
-    current_file <- read.csv(file_paths[i], header=T, sep=",")
-    corr_vector[j] <- cor(current_file$sulfate, current_file$nitrate, use="complete.obs")
+  for(i in idnums) {
+    polldata <- read.csv(fileList[i])
+    corrdata[j] <- cor(polldata$sulfate, polldata$nitrate, use="complete.obs")
     j <- j + 1
   }
-  result <- corr_vector
+  result <- corrdata
   return(result)   
 }
-
 
     
